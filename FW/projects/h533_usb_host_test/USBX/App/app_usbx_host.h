hanyazou/Nucleo64_Z80

fw: h533_usb_host_test: Fix USBX configuration parameters

FW/projects/h533_usb_host_test/USBX/App/app_usbx_host.h
@@ -41,7 +41,7 @@ extern "C" {
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
-#define USBX_HOST_MEMORY_STACK_SIZE     1024
+#define USBX_HOST_MEMORY_STACK_SIZE     0x5000
 
 #define UX_HOST_APP_THREAD_STACK_SIZE   1024
 #define UX_HOST_APP_THREAD_PRIO         10

hanyazou/Nucleo64_Z80-H533

FW/projects/h533_usb_host_test/USBX/App/app_usbx_host.h
@@ -40,7 +40,7 @@ extern "C" {
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
-#define USBX_HOST_MEMORY_STACK_SIZE     1024
+#define USBX_HOST_MEMORY_STACK_SIZE     0x8000
 
 #define UX_HOST_APP_THREAD_STACK_SIZE   1024
 #define UX_HOST_APP_THREAD_PRIO         10
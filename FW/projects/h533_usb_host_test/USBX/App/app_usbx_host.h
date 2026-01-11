aac0daa10de305fe59cbc76d6e7a4fbcd4784c86
hanyazou <hanyazou@gmail.com>
2026-01-11 12:41:51 +0900

    fw: h533_usb_host_test: Fix USBX configuration parameters

--- FW/projects/h533_usb_host_test/USBX/App/app_usbx_host.h
+++ FW/projects/h533_usb_host_test/USBX/App/app_usbx_host.h
@@ -40,7 +40,7 @@ extern "C" {
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
-#define USBX_HOST_MEMORY_STACK_SIZE     1024
+#define USBX_HOST_MEMORY_STACK_SIZE     0x8000
 
 #define UX_HOST_APP_THREAD_STACK_SIZE   1024
 #define UX_HOST_APP_THREAD_PRIO         10
